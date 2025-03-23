3aea373f1dd024a96713a46d39f49dd70a306b66
Filip <101202559+ClanyX@users.noreply.github.com>
2025-03-23 13:22:10 +0100

    last edit before first start

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -3,7 +3,7 @@ import Database from 'better-sqlite3';
 export const db = new Database('src/lib/server/database.sqlite');
 
 db.exec(`
-    CREATE TABLE IF NOT EXITS users(
+    CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
