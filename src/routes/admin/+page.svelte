78cce83b8f5e6b1f27b5b74d6498ed8a94afc357
Filip <101202559+ClanyX@users.noreply.github.com>
2025-03-23 14:09:36 +0100

    edited part

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-    import { page } from '$app/stores';
-
-    $: if (!$page.data.user || $page.data.user.role !== 'admin') {
-        window.location.href = '/user';
-    }
-
     async function logout(){
         await fetch('api/users/logout', { method: 'POST' });
         window.location.href = '/login';
@@ -12,5 +6,5 @@
 </script>
 
 <h1>Admin Page</h1>
-<p>Welcome, {$page.data.user?.username}!</p>
+<p>Welcome, admin!</p>
 <button on:click={logout}>Logout</button>
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,10 +1,2 @@
-<script lang="ts">
-    import { page } from '$app/stores';
-
-    $: if (!$page.data.user) {
-        window.location.href = '/login';
-    }
-</script>
-
-<h1>Welcome, {$page.data.user?.username}!</h1>
+<h1>Welcome, user!</h1>
 <p>This is your dashboard.</p>
