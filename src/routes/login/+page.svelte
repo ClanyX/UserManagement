e024aac64f84ae6c9a0e437e78d915faa5a3101a
Filip <101202559+ClanyX@users.noreply.github.com>
2025-03-24 23:52:10 +0100

    remove role

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
     let username = "";
     let password = "";
-    let role = "";
     let errorMessage = "";
 
     async function login() {
         const res = await fetch("/api/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, password, role }),
+            body: JSON.stringify({ username, password }),
         });
 
         const data = await res.json();
