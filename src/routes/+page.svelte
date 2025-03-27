d783b43c4b8d3deb69bf8e6a17e843ac4e4b3765
Filip <101202559+ClanyX@users.noreply.github.com>
2025-03-27 19:27:13 +0100

    tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,24 +10,9 @@
     }
 </script>
 
-<main class="container">
-    <h1>Vítejte v aplikaci</h1>
+<main class="container text-center mx-auto mt-5 text-red-500">
+    <h1>Vítejte, uživateli!</h1>
 
     <a href="/login">Přihlásit se</a>
     <a href="/register">Registrovat se</a>
 </main>
-
-<style>
-    .container {
-        max-width: 600px;
-        margin: 0 auto;
-        text-align: center;
-    }
-    a {
-        display: block;
-        margin: 10px;
-        text-decoration: none;
-        font-size: 18px;
-        color: #007bff;
-    }
-</style>
